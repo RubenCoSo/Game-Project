4906e4b373cf530e8cf04193f9b45db42626d28c
Ruben Corral <rubencorralsola@gmail.com>
2021-08-26 15:46:39 +0200

    proyecto semidefinitivo

--- js/gameApp.js
+++ js/gameApp.js
@@ -1,30 +1,63 @@
 const gameApp = {
+  //canvas settings
   ctx: undefined,
   canvasSize: {
     w: undefined,
     h: undefined,
   },
+
+  //player Image
   magusSize: {
     w: 80,
-    h: 80,
+    h: 100,
   },
+  wizardIndexX: 0,
+  wizardIndexY: 0,
+  wizImgW: 32,
+  wizImgH: 48,
+  magusMovement: false,
+
+  //game settings
   intervalId: undefined,
   score: 0,
   gems: [],
   MAX_Gems: 5,
   frameCounter: 0,
-  timerCounter: 1000,
-  referenceGem: undefined,
+  timerCounter: 10,
+
+  //explossion
+  explosionRef: undefined,
+  expIndX: 0,
+  expIndY: 0,
+  expW: 10,
+  expH: 10,
+  expSize: {
+    W: 100,
+    h: 100,
+  },
+  crystalSound: document.getElementById("pickAGem"),
+  intervalExp: undefined,
+  refGem: undefined,
+
+  // //DOM constants
+  // counter: document.getElementById("counter"),
+  // timer: document.getElementById("timer"),
 
   init(canvas) {
     this.getContext(canvas);
     this.setCanvasDimension(canvas);
+    // this.timer.appendChild(this.timerHTML);
     this.newMap();
     this.magus = new Image();
-    this.magus.src = "../images/wizard2.png";
+    this.magus.src = "../images/mage.png";
     this.setListeners();
     this.createGems();
-    console.log(this.gems);
+    this.explosion = new Image();
+    this.explosion.src = "../images/explossion.png";
+    // this.crystalSound = sound(
+    //   "../audio/GemCrystal Collect Sound Effect Crash Bandicoot N. Sane Trilogy.mp3"
+    // );
+    // console.log(this.gems);
     this.screenRefresh();
   },
 
@@ -47,11 +80,20 @@ const gameApp = {
   drawMagus() {
     this.ctx.drawImage(
       this.magus,
+      this.wizardIndexX * this.wizImgW,
+      this.wizardIndexY * this.wizImgH,
+      this.wizImgW,
+      this.wizImgH,
       this.canvasSize.w / 2 - this.magusSize.w / 2,
       this.canvasSize.h / 2 - this.magusSize.h / 2,
       this.magusSize.w,
       this.magusSize.h
     );
+    if (this.frameCounter % 10 === 0) {
+      this.wizardIndexX < 3 && this.magusMovement
+        ? (this.wizardIndexX += 1)
+        : (this.wizardIndexX = 0);
+    }
   },
 
   screenRefresh() {
@@ -62,7 +104,6 @@ const gameApp = {
       this.pickUpGems();
       this.showScores();
       this.frameCounter++;
-      // console.log(this.map.mapPosition);
       if (this.frameCounter % 100 === 0) {
         this.timerCounter--;
         this.filterGem();
@@ -85,15 +126,23 @@ const gameApp = {
       switch (e.key) {
         case "a":
           this.map.moveLeft = true;
+          this.wizardIndexY = 1;
+          this.magusMovement = true;
           break;
         case "w":
           this.map.moveUp = true;
+          this.wizardIndexY = 3;
+          this.magusMovement = true;
           break;
         case "d":
           this.map.moveRight = true;
+          this.wizardIndexY = 2;
+          this.magusMovement = true;
           break;
         case "s":
           this.map.moveDown = true;
+          this.wizardIndexY = 0;
+          this.magusMovement = true;
           break;
       }
     });
@@ -101,15 +150,19 @@ const gameApp = {
       switch (e.key) {
         case "a":
           this.map.moveLeft = false;
+          this.magusMovement = false;
           break;
         case "w":
           this.map.moveUp = false;
+          this.magusMovement = false;
           break;
         case "d":
           this.map.moveRight = false;
+          this.magusMovement = false;
           break;
         case "s":
           this.map.moveDown = false;
+          this.magusMovement = false;
           break;
       }
     });
@@ -138,9 +191,9 @@ const gameApp = {
         gem.gemPosition.y + this.map.mapPosition.y <
           this.map.mapPosition.y + 2400
     );
-    console.log(this.gems);
+    // console.log(this.gems);
     this.gems.length < this.MAX_Gems ? this.createGems() : null;
-    console.log(this.gems);
+    // console.log(this.gems);
   },
 
   pickUpGems() {
@@ -159,13 +212,54 @@ const gameApp = {
           this.map.mapPosition.y + gem.gemPosition.y
       ) {
         this.score += 1;
-        this.gems.splice(i, 1);
+        this.refGem = this.gems.splice(i, 1);
+        let pickaGem = document.getElementById("pickAGem");
+        console.log(pickaGem);
+        pickaGem.play();
+
         // console.log(this.gems);
         // console.log(this.referenceGem)
         // console.log(this.gems);
-        this.timerCounter += 5;
+        this.score < 10 ? (this.timerCounter += 3) : null;
+        this.score < 15 ? (this.timerCounter += 2) : null;
+        this.score < 20 ? (this.timerCounter += 1) : null;
+
+        this.gemExplosion(this.refGem[0]);
       }
     });
+  },
+
+  gemExplosion(gem) {
+    let expCounter = 0;
+    this.intervalExp = setInterval(() => {
+      this.ctx.clearRect(0, 0);
+      this.ctx.drawImage(
+        this.explosion,
+        this.expIndX * this.expW,
+        this.expIndY * this.expH,
+        this.expW,
+        this.expH,
+        this.map.mapPosition.x + gem.gemPosition.x,
+        this.map.mapPosition.y + gem.gemPosition.y,
+        this.expSize.w,
+        this.expSize.y
+      );
+
+      expCounter += 1;
+      this.expIndX += 1;
+      if (this.expIndX > 9) {
+        this.expIndY = +1;
+        this.expIndX = 0;
+      }
+      if (this.expIndY > 7) {
+        this.expIndY = 0;
+      }
+      console.log(gem);
+      console.log("rula");
+    }, 100);
+    setTimeout(() => {
+      clearInterval(this.intervalExp);
+    }, 2000);
   },
 
   showScores() {
@@ -175,8 +269,27 @@ const gameApp = {
   },
 
   timer() {
+    this.minutes;
+    this.seconds;
     this.ctx.font = "30px Verdana";
-    this.ctx.fillText("Timer: " + this.timerCounter, 100, 100);
+    if (this.timerCounter < 60) {
+      this.ctx.fillText("Timer: 00:" + this.timerCounter, 100, 100);
+    } else {
+      minutes = this.timerCounter / 60;
+      seconds = this.timerCounter % 60;
+      minutes < 9 ? (minutes = `0${minutes}`) : (minutes = `${minutes}`);
+      seconds.length === 1
+        ? (seconds = `0${seconds}`)
+        : (seconds = `${seconds}`);
+      this.ctx.fillText("Timer:" + minutes + ":" + seconds, 100, 100);
+    }
+
+    // this.timerHTML = document.createElement("p");
+    // this.node = document.createTextNode(
+    //   `"Timer:  ${this.minutes}:${this.seconds}`
+    // );
+    // this.timerHTML.appenchild(this.node);
+    // this.timerHTML.setAttribute("class", "gametimer");
   },
 
   addZeros(num) {
--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 window.onload = () => {
-  document.getElementById("startGame").onclick = () => {
+  const playPause = document.getElementById("playpause");
+  const music = document.getElementById("music");
+  const startGame = document.getElementById("startGame");
+  const instructions = document.getElementById("instructions");
+
+  playPause.addEventListener("click", () => {
+    if (playPause.getAttribute("class") === "pause") {
+      playPause.setAttribute("class", "play");
+      playPause.src = "../images/play.png";
+      music.setAttribute("autoplay", "false");
+    } else if (playPause.getAttribute("class") === "play") {
+      playPause.setAttribute("class", "pause");
+      playPause.src = "../images/pause.png";
+      music.setAttribute("autoplay", "true");
+    }
+  });
+
+  startGame.onclick = () => {
     const canvas = document.querySelector("#canvas");
     canvas.style.display = "block";
     gameApp.init(canvas);
+    startGame.remove();
+    instructions.remove();
   };
 };
